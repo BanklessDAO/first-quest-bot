50e8e6dae999d92ede5070b420503cb6853f55e3
nonsensetwice <joshua@nonsensecodes.io>
2023-05-19 13:42:38 -0700

    comment out unused code.

--- src/app/events/first-quest/StartFirstQuestFlow.ts
+++ src/app/events/first-quest/StartFirstQuestFlow.ts
@@ -1,8 +1,13 @@
 import channelIds from '../../service/constants/channelIds';
-import { GuildMember, Message, MessageEmbed, TextChannel } from 'discord.js';
+import {
+	GuildMember,
+	Message,
+	// MessageEmbed,
+	TextChannel
+} from 'discord.js';
 import Log from '../../utils/Log';
 import { addNewUserToDb, sendFqMessage } from '../../service/first-quest/LaunchFirstQuest';
-import roleIds from '../../service/constants/roleIds';
+// import roleIds from '../../service/constants/roleIds';
 
 const StartFirstQuestFlow = async (guildMember: GuildMember): Promise<void> => {
 	Log.debug(`starting first quest flow for new user ${guildMember.user.tag}`);
